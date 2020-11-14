c1f238d8b5da5002260829751da4b0db41f65420
SadiaJahangir <sadiajahangir.2397@gmail.com>
2020-11-15 02:55:55 +0600

    CustomerHomeActivity

--- SecureDelivery/app/src/main/java/com/example/securedelivery/CustomerHomeActivity.java
+++ SecureDelivery/app/src/main/java/com/example/securedelivery/CustomerHomeActivity.java
@@ -2,7 +2,10 @@ package com.example.securedelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CustomerHomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class CustomerHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+    }
+
+    public void placeordercamera(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "camera ordered", Toast.LENGTH_SHORT).show();
+    }
+
+    public void placeorderphone(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "phone ordered", Toast.LENGTH_SHORT).show();
+    }
+
+    public void placeordermouse(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "mouse ordered", Toast.LENGTH_SHORT).show();
+    }
+
+    public void placeorderSoundBox(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "soundbox ordered", Toast.LENGTH_SHORT).show();
+    }
+
+    public void placeorderheadphone(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "headphone ordered", Toast.LENGTH_SHORT).show();
+    }
+
+    public void placeorderwatch(View view) {
+        Toast.makeText(CustomerHomeActivity.this, "watch ordered", Toast.LENGTH_SHORT).show();
     }
 }
