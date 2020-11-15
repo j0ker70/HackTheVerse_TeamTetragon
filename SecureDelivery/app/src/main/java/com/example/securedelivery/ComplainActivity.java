f394b6c3dd9a91c1d895097394a24fe3a58c469a
mdreaj38 <mdreaj38@gmail.com>
2020-11-15 06:27:19 +0600

    Last

--- SecureDelivery/app/src/main/java/com/example/securedelivery/ComplainActivity.java
+++ SecureDelivery/app/src/main/java/com/example/securedelivery/ComplainActivity.java
@@ -3,12 +3,21 @@ package com.example.securedelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ComplainActivity extends AppCompatActivity {
+
+    ListView simpleList;
+    String countryList[] = {"Complain name1:Worker1","Complain name2:Worker2","Complain name3:Worker3","Complain name4:Worker4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complain);
+
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.acticity_main_list, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
     }
 }
--- SecureDelivery/app/src/main/java/com/example/securedelivery/WorkerHomeActivity.java
+++ SecureDelivery/app/src/main/java/com/example/securedelivery/WorkerHomeActivity.java
@@ -25,7 +25,7 @@ public class WorkerHomeActivity extends AppCompatActivity {
 
     private GridView gridView;
     String[] Options = {"Order List", "Submit Transaction", "History", "Profile"};
-    int[] OptionImage = {R.drawable.order_list, R.drawable.payment, R.drawable.order_list, R.drawable.order_list};
+    int[] OptionImage = {R.drawable.order_list, R.drawable.payment, R.drawable.history, R.drawable.profile};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_home);
